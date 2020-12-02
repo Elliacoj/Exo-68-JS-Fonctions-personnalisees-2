a6dcb7c1bc4c4f18abf66794257ea9acda2456f9
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-02 15:30:58 +0100

    Modif

---
+++ app.js
@@ -0,0 +1,9 @@
+function random1() {
+    return  Math.trunc(Math.random() * 100);
+
+}
+
+let element = document.getElementsByTagName('p')
+for (let i = 0; i < element.length; i++) {
+    element[i].innerHTML = random1();
+}
